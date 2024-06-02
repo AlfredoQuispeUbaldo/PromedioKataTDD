708ecc15d490323b284df17a304e4253d77b4fef
AlfredoQuispeUbaldo <71438344@continental.edu.pe>
2024-06-02 16:54:17 -0500

    Caso 4: Calcular promedio numeros vacios (Verde)

--- src/logica/Conjunto.py
+++ src/logica/Conjunto.py
@@ -1,6 +1,8 @@
 def calcular_promedio_ponderado(numeros, pesos):
     if len(numeros) != len(pesos):
         raise ValueError("Las listas de números y pesos deben tener la misma longitud.")
+    if len(numeros) == 0 or len(pesos) == 0:
+        raise ValueError("Las listas de números y pesos no pueden estar vacías.")
     if any(p == 0 for p in pesos):
         raise ValueError("Los pesos no pueden contener valores cero.")
 
