a5a28b535762d0878502f47b6f6c606f95c78ca4
AlfredoQuispeUbaldo <71438344@continental.edu.pe>
2024-06-02 16:49:05 -0500

    Caso 2: Calcular promedio contienen cero (Verde)

--- src/logica/Conjunto.py
+++ src/logica/Conjunto.py
@@ -1,6 +1,8 @@
 def calcular_promedio_ponderado(numeros, pesos):
     if len(numeros) != len(pesos):
         raise ValueError("Las listas de números y pesos deben tener la misma longitud.")
+    if any(p == 0 for p in pesos):
+        raise ValueError("Los pesos no pueden contener valores cero.")
 
     suma_ponderada = sum(n * p for n, p in zip(numeros, pesos))
     suma_pesos = sum(pesos)
