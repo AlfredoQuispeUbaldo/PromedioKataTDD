ce9367f31d505d0828f9d4842e31442459d06286
AlfredoQuispeUbaldo <71438344@continental.edu.pe>
2024-06-02 16:33:15 -0500

    Caso 1: Calcular promedio ponderado cp0 (Verde)

--- src/logica/Conjunto.py
+++ src/logica/Conjunto.py
@@ -1,2 +1,4 @@
 def calcular_promedio_ponderado(numeros, pesos):
-    pass
+    suma_ponderada = sum(n * p for n, p in zip(numeros, pesos))
+    suma_pesos = sum(pesos)
+    return suma_ponderada / suma_pesos
