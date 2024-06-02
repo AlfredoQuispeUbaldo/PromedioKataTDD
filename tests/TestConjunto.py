78f92e7b61e566b69c695be365d7b0b1640a40f7
AlfredoQuispeUbaldo <71438344@continental.edu.pe>
2024-06-02 16:30:47 -0500

    Caso 1: Calcular promedio ponderado cp0 (Rojo)

--- tests/TestConjunto.py
+++ tests/TestConjunto.py
@@ -1,7 +1,12 @@
 import unittest
+from src.logica.Conjunto import calcular_promedio_ponderado
 
-class TestConjunto():
-    pass
+class TestConjunto(unittest.TestCase):
+    def test_promedio_ponderado_cp0(self):
+        numeros = [10, 12, 14]
+        pesos = [3, 4, 2]
+        resultado_esperado = 11.78
+        self.assertAlmostEqual(calcular_promedio_ponderado(numeros, pesos), resultado_esperado, places=2)
 
 if __name__ == '__main__':
     unittest.main()
